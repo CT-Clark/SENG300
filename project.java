96b317e1e0591d5be6fa3d993ed90db23be50e11
sceveleigh <sceveleigh@gmail.com>
2018-03-08 14:01:48 -0700

    Added simple code
    In Eclipse to set command line arguments:
    -Click the drop down menu to the right of the run project button
    -Click "run configurations.."
    -Click the "(x)= Arguments" tab
    -Enter arguments seperated by spaces

--- project.java
+++ project.java
@@ -5,8 +5,25 @@
  *
  */
  
-public class Project {
+public class project {
+	private static String pathName;
+	private static String javaTypeName;
     public static void main(String[] args) {
-  
+    	int declarationsFound = 0;
+    	int referencesFound = 0;
+    	
+    	
+    	try {
+    		pathName = args[0];
+    		javaTypeName = args[1];
+    	} catch (ArrayIndexOutOfBoundsException e) {
+    		
+    	}
+    	
+    	//declarationsFound =
+    	//referencesFound = 
+    	
+    	System.out.println(javaTypeName + ". Declarations found: " + 
+    			declarationsFound + "; references found: " + referencesFound);
     }
 }
