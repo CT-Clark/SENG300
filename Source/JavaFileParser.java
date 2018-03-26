a88f3473cd19b39df085ad69c8caa9036fe6ac24
brandt-davis <37000680+brandt-davis@users.noreply.github.com>
2018-03-26 01:47:31 -0600

    Add files via upload

--- Source/JavaFileParser.java
+++ Source/JavaFileParser.java
@@ -1,10 +1,8 @@
 package astParserProject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.*;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.jdt.core.JavaCore;
@@ -19,32 +17,33 @@ public class JavaFileParser {
 	 * 
 	 * @param names An array containing the names of .java files in sourcePath, the names must include the .java extension
 	 * @param sourcePath A string representing the path of the files being considered
-	 * @return A hash map containing the types as keys and an array of the number of declarations and references as the information
+	 * @param typeName A string containing the fully qualified name of the type whose declarations and references are being counted
+	 * @return An integer array of length 2 with the first element containing the number of declarations of typeName counted,
 	 * and the second element containing the number of references to typeName counted.
 	 * @throws IOException If an I/O error occurs while opening or reading the file
 	 */
-	public HashMap<String, int[]> parse(String[] names, String sourcePath) throws IOException {
-		
-		// Initilize the parser and visitor
+	public int[] parse(String[] names, String sourcePath, String typeName) throws IOException {
 		parser = ASTParser.newParser(AST.JLS8);
-		TypeFinderVisitor typeFinder = new TypeFinderVisitor();
-		
-		
-		// For loop to iterate through each .java file
+		int[] countPair = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 		for (int i = 0; i < names.length; i++) {
-			
 			// Configure the parser.
+			
+			
 			parser.setResolveBindings(true);
 			parser.setBindingsRecovery(true);
+			
+			//Map<String, String> options = JavaCore.getOptions();
 			Map options = JavaCore.getOptions();
 			JavaCore.setComplianceOptions(JavaCore.VERSION_1_5, options);
 			parser.setCompilerOptions(options);
+			
 			parser.setEnvironment(new String[] {}, new String[] { sourcePath }, null, true);
 			
 			
 			
 			// Create the path and set the unit name
-			String path = sourcePath + "\\" + names[i];
+			String path = sourcePath + "/" + names[i];
 			parser.setUnitName(names[i]);
 
 			// Read the contents of the given .java file into a StringBuilder object
@@ -64,14 +63,21 @@ public class JavaFileParser {
 			
 			// Create the ast for the input file
 			CompilationUnit ast = (CompilationUnit) parser.createAST(null);
-			
+			TypeFinderVisitor typeFinder = new TypeFinderVisitor();
+			typeFinder.setTargetType(typeName);
 			ast.accept(typeFinder);
 			
 			
 			
+
+			for (int j = 0; j<10;j++){
+				
+				countPair[j] += typeFinder.refsAndDecsCount[j];
+			}
 		}
-		// Return the hash map made from the parser
-		return typeFinder.hMap;
+	
+
+		return countPair;
 	}
 
 }
